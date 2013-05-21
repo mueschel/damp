901c91f4ddcf6f5147a91f074ec25f2e53f098f8
Jan <jan@mueschelsoft.de>
2013-05-21 23:22:15 +0200

    performance tuning - hope that works

--- damp.c
+++ damp.c
@@ -19,16 +19,17 @@
 
 //CKSEL fuses should be 0b0001 - PLL as source
 
-
+#define FREQMODE 1
+#define AUDIOMODE 0
 #define DACP OCR1A
 #define DACN OCR1B
 #define DACT TC1H
 
-volatile uint8_t mode_select = 0; //1: frequency generator, 0: external sound input
+volatile uint8_t mode_select = AUDIOMODE; //1: frequency generator, 0: external sound input
 
-volatile uint16_t period = 48;    //Compare register for frequency setting. 128 overflows to one period
 volatile uint8_t amplitude = 1;  //amplitude
-volatile int16_t real_value = 0;
+volatile uint16_t real_value_pos = 512;
+volatile uint16_t real_value_neg = 512;
 
 #define ADMUX_AUDIO         (1<<REFS0) | (1<<REFS1) | (0<<ADLAR) | 0   //input 0, reference internal 2.56V with capacitor
 #define ADCSRA_AUDIO        (1<<ADEN) | (1<<ADSC) | (1<<ADATE) | (1<<ADIE) | (4<<ADPS0)
@@ -52,7 +53,7 @@ __attribute__((naked)) int main(void)
     ADCSRB = (1<<REFS2);
 
 	// Timer0: Signal generation (CTC /256)
-	OCR0A = period; //interval to generate interrupt
+	OCR0A = 10; //interval to generate interrupt
     TCCR0A = (1<<0); //CTC mode  0: CTC0
     TCCR0B = TCCR0B_AUDIO;
     TIMSK |= (1<<OCIE0A); //interrupt on compare match / counter reset
@@ -82,14 +83,14 @@ __attribute__((naked)) int main(void)
 		_delay_us(100);
         
         if(mode_select != ((~PINB) & 1) ) {
-            if(mode_select == 0) {  //change to generator
-                mode_select = 1;
+            if(mode_select == AUDIOMODE) {  //change to generator
+                mode_select = FREQMODE;
                 TCCR0B = TCCR0B_FREQ;
 	            ADMUX  = ADMUX_FREQ;
 	            ADCSRA = ADCSRA_FREQ;
                 }
             else {                  //change to Audio
-                mode_select = 0;
+                mode_select = AUDIOMODE;
                 TCCR0B = TCCR0B_AUDIO;
 	            ADMUX  = ADMUX_AUDIO;
 	            ADCSRA = ADCSRA_AUDIO;
@@ -101,10 +102,10 @@ __attribute__((naked)) int main(void)
 //mode 1: switch between ADC 5 (vol) and 6 (freq), set frequency and amplitude registers, two reads per input
 //mode 0: copy ADC value to real_value for PWM
 ISR(ADC_vect) {
-
+    static uint16_t period = 160;
 	static uint8_t channel = 5;
 	static uint8_t valid = 0; //accept only second read from each input.
-	if(mode_select == 1) { // read 
+	if(mode_select == FREQMODE) { // read 
 		if(valid == 0) {
 			valid = 1;
 		} else {		
@@ -113,9 +114,9 @@ ISR(ADC_vect) {
 			if (channel==6) {
     			ADMUX = (ADMUX & 0xF0) | 5;
 				period -= period/4;
-				period += ADC/4;
-				if (period < 40) period = 40;  //upper limit to prevent too high interrupt load
-				OCR0A = period/4;
+				period += ADC;
+				if (period < 240) period = 240;  //upper limit to prevent too high interrupt load
+				OCR0A = period/16;
 				channel = 5;
 			} else {
     			ADMUX = (ADMUX & 0xF0) | 6;
@@ -125,21 +126,27 @@ ISR(ADC_vect) {
 		    }	
 	    }		
     else {
-        real_value = ADC -512;
+        uint16_t real_value;
+        real_value     = ADC;
+	    real_value_pos = real_value;
+	    real_value_neg = 1023 - real_value;
+
         }
 		
 }	
 
-//called at F_CPU/256*period 
-//40<period<255 --> 3.8 - 97 Hz
+//called at F_CPU/64*period, run only every second time.
+//10<period<255 --> 3.8 - 97 Hz
 ISR(TIMER0_COMPA_vect) {
 	sei();
-    //PORTB ^= (1<<4);
-    
+    static uint8_t time = 0;
 	static int8_t direction = 1;  //ramp up or down
     static int8_t value = 0;
     static uint8_t cur_amplitude = 0;
     static int16_t tmp_value = 0;
+    
+    if(++time & 1)
+        return;
 
     // Amplitude changes at 0 only
     if (value == 0) 
@@ -147,11 +154,7 @@ ISR(TIMER0_COMPA_vect) {
 
 	value = value + direction;
 
-#if 0 //standard arithmetic
-	real_value = ((int16_t)value * cur_amplitude) / 8;
-
-
-#else //optimized without multiplication.
+	//real_value = ((int16_t)value * cur_amplitude) / 8;
     if (value == 0) {
         tmp_value = 0; //reset to correct 0 value
         }
@@ -161,29 +164,29 @@ ISR(TIMER0_COMPA_vect) {
         else
             tmp_value = tmp_value - cur_amplitude;
         }           
-    real_value = tmp_value /8;
-#endif
 
 	if(value <= -63 || value >= 63)
     	direction = -direction;
 
+    uint16_t real_value = 1024 + tmp_value /16;
+    uint16_t val1 = real_value >> 1;
+    uint16_t val2 = real_value & 1;
+    if(val1 > 1023)
+        val1 = 1023;
+    if(val1 < 0)
+        val1 = 0;
+    real_value_pos = val1 + val2;
+    real_value_neg = 1023 - val1;
 }
 
 	
 
 ISR(TIMER1_OVF_vect) {
-	int16_t val1	= real_value ;//>> 1;
-    uint8_t val2    = real_value & 0;
     uint16_t pos;
     uint16_t neg;
-	if(val1 > 511)
-	    val1 = 511;
-	else if(val1 < -512)
-	    val1 = -512;
 
-	pos = 511 + val1 + val2;
-	neg = 511 - val1;
-
+    pos = real_value_pos;
+    neg = real_value_neg;
 
     DACT = (pos >> 8);
 	DACP = pos;
